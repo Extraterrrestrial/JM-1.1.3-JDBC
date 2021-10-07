285b361393938b700d9913b995c0eed9f25b2f34
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-07 22:06:34 +0600

    Override toString

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,31 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
+    public void createUsersTable() {
     }
 
     public void dropUsersTable() {
-
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
     }
 
     public void removeUserById(long id) {
-
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        userDaoJDBC.cleanUsersTable();
     }
 }
