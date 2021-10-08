86fccddac5ed2911406dbe85b8d7ba6cc38facb0
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-09 01:12:40 +0600

    class UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,19 +9,22 @@ public class UserServiceImpl implements UserService {
     UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-
         return userDaoJDBC.getAllUsers();
     }
 
